8415b1e627bbbde7646b63d48d1ee3f6358e2c8b
Peter Vanhee <peter.vanhee@gmail.com>
2016-10-17 14:11:35 +0200

    Add another die that flips on the same flip button

--- examples/04-random.elm
+++ examples/04-random.elm
@@ -19,13 +19,14 @@ main =
 
 
 type alias Model =
-  { dieFace : Int
+  { dieFace1 : Int
+  , dieFace2 : Int
   }
 
 
 init : (Model, Cmd Msg)
 init =
-  (Model 1, Cmd.none)
+  (Model 1 1, Cmd.none)
 
 
 
@@ -34,17 +35,18 @@ init =
 
 type Msg
   = Roll
-  | NewFace Int
+  | NewFace (Int, Int)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Roll ->
-      (model, Random.generate NewFace (Random.int 1 6))
+      (model, Random.generate NewFace (Random.pair (Random.int 1 6) (Random.int 1 6)))
 
-    NewFace newFace ->
-      (Model newFace, Cmd.none)
+    NewFace (newFace1, newFace2) ->
+      ({ model | dieFace1 = newFace1, dieFace2 = newFace2 }, Cmd.none)
+      --(Model newFace1, newFace2, Cmd.none)
 
 
 
@@ -63,6 +65,7 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   div []
-    [ h1 [] [ text (toString model.dieFace) ]
+    [ h1 [] [ text (toString model.dieFace1) ]
+    , h1 [] [ text (toString model.dieFace2)]
     , button [ onClick Roll ] [ text "Roll" ]
     ]
